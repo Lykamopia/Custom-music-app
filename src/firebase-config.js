e3f02594b14d95b23d3fd2626b2e28dc88710574
Lykamopia <www.alex94lykam@gmail.com>
2023-06-21 11:34:50 +0300

    configuring confidential

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore"
 
 const firebaseConfig = {
-    apiKey:process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+    apiKey:process.env.REACT_APP_API_KEY,
+    authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId:process.env.REACT_APP_APP_ID,
+    measurementId:process.env.REACT_APP_MEASUREMENT_ID
   };
-
+  console.log(process.env)
   const app = initializeApp(firebaseConfig);
 
   export const db = getFirestore(app);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
 import store from './app/store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
