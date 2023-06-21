78327d887550412bb53dc11bebe1fb3eb05b02b3
Lykamopia <www.alex94lykam@gmail.com>
2023-06-21 12:02:33 +0300

    firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
     appId:process.env.REACT_APP_APP_ID,
     measurementId:process.env.REACT_APP_MEASUREMENT_ID
   };
-  console.log(process.env)
   const app = initializeApp(firebaseConfig);
 
   export const db = getFirestore(app);
